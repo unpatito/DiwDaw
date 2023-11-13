4629a73e4c6ddd30c4076eab8a7bdaaca2679244
daw <lutacocas@gmail.com>
2023-11-13 20:01:08 +0100

    Añadido cursoAnimaciones

---
+++ cursoAnimaciones/animacion.js
@@ -0,0 +1,12 @@
+window.onload = function (){
+
+    document.getElementsByClassName("chincheta")[0].classList.add("chincheta1");
+
+    document.getElementsByTagName("article")[0].classList.add("article1");
+
+    document.getElementsByClassName("chincheta")[1].classList.add("chincheta2");
+    document.getElementsByClassName("chincheta")[2].classList.add("chincheta3");
+
+    document.getElementsByTagName("article")[2].classList.add("article3");
+
+}
